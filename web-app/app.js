6e4ac43ffe95765861f2e139e13764b30e09d72c
jstevensstein <jstevensstein@showingtime.com>
2017-09-26 14:05:05 -0500

    now using pug for views

--- web-app/app.js
+++ web-app/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use("/vendor", express.static(path.join(__dirname, "vendor")));
 
-// viewed at http://localhost:8080
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.render("index");
 });
 
-app.listen(8080);
+app.listen(3000);
