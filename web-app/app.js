1bd538511d7d2bbdaa5679815e572af2f6463da9
jstevensstein <jstevensstein@showingtime.com>
2017-09-27 17:49:51 -0500

    Revert "now touches mysql db"
    This reverts commit 5a026cc44ca8a959602596c67a081af978b93072.

--- web-app/app.js
+++ web-app/app.js
@@ -1,25 +1,18 @@
 var express = require('express');
-var bodyParser = require('body-parser')
-var path = require('path');
-
-var PollsService = require('./pollsService.js');
-
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
-
+var path = require('path');
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use("/vendor", express.static(path.join(__dirname, "vendor")));
 
 
 app.get('/', function(req, res) {
-  res.render("index");
+    res.render("index");
 });
 
-app.post('/createpoll', urlencodedParser, function(req, res){
-  PollsService.createPoll(req.body.name, req.body.choices);
-  console.log('returned from createPoll');
-  res.status(200).json(null);
+app.post('/createpoll', function(req, res){
+	console.log('hit createpoll method');
+	res.send('eh');
 });
 
 app.listen(3000);
