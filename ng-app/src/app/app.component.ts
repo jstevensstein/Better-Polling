8789961683733cde0ef9080bfd0e29834cb5a46b
jstevensstein <jstevensstein@gmail.com>
2017-11-12 18:53:23 -0600

    angular app now has most poll-building functionality

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Directive, OnInit, QueryList, ViewChildren, Input } from '@angular/core';
+import {MatInputModule, MatInput, MatIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+        'delete',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/ic_delete_24px.svg')
+      );
+  }
+
+  @ViewChild('foobuz') foobuz: MatInput;
+  @ViewChildren('foobar') foobar: QueryList<MatInput>;
+
+  pollOptions: string[] = [{value:'Option 1'}, {value: 'Option 2'}];
+
+  addOptionAtEnd = function(){
+    this.addOption(this.pollOptions.length);
+  }
+
+  addOption = function(i: number){
+    this.pollOptions.splice(i, 0, {value: `Option ${this.pollOptions.length + 1}`});
+    setTimeout(() =>
+      this.foobar._results[i].nativeElement.select();
+    );
+  }
+
+  onEnter = function(i : number){
+    this.addOption(i+1);
+  }
+
+  onDelete = function(i : number){
+    if (!this.pollOptions[i].value){
+      this.removeOption(i);
+    }
+  }
+
+  removeOption = function(i : number){
+    this.pollOptions.splice(i, 1);
+    setTimeout(() =>
+      this.foobar._results[i-1].nativeElement.select();
+    );
+  }
+
+  onBlurOption = function(i : number){
+    this.pollOptions[i].value = this.pollOptions[i].value || `Option ${i + 1}`;
+  }
 }
--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule, MatIconRegistry} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 
@@ -10,9 +12,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    FormsModule,
+    MatIconModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MatIconRegistry
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
